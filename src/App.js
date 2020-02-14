5fe79d890631734fe30bebd742d475c2195e0b5d
Felipe Bianchi <fbs96@live.com>
2020-02-14 14:22:49 -0300

    Arquivos até o término da aula Criando Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
 
+const App = () => 
+(
+  <div className="App">
+      <Header />
+  </div>
+);
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
-}
 
 export default App;
