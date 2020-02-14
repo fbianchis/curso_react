2f066280e9185f1a87caba73d30ac06963c9f231
Felipe Bianchi <fbs96@live.com>
2020-02-14 15:25:03 -0300

    Arquivos até o término da aula Armazenando no estado

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,18 +2,32 @@ import React, { Component } from 'react';
 import api from '../../services/api'
 export default class Main extends Component
 {
+    state = 
+    {
+        products : [],
+    }
     componentDidMount()
     {
         this.loadProducts()
     }
     render()
     {
-        return <h1>Hello World</h1>;
+        return (
+            <div className = "product-list">
+                {this.state.products.map(product => 
+                (
+                    <h2 key={product._id}>
+                        {product.title}
+                    </h2>
+                ))}
+            </div>
+        )
     }
     // para funções que não são da classe, utiliza-se arrow func para poder utilizar this da classe que se é extendidada
     loadProducts = async () => 
     {
         const response = await api.get('./products');
+        this.setState({products : response.data.docs});
         console.log(response.data.docs);
     };
 }
